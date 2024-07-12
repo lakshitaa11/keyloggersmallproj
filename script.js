86defa94d8de3ce741cf1f9e661b3487bd8e3adf
Lakshita <97162780+lakshitaa11@users.noreply.github.com>
2024-07-12 21:51:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const logDiv = document.getElementById("log");
+const stateDiv = document.getElementById("state");
+const startBtn = document.getElementById("start-btn");
+const stopBtn = document.getElementById("stop-btn");
+
+startBtn.addEventListener("click", () => {
+    document.addEventListener("keydown", handleDown);
+    document.addEventListener("keyup", handleUp);
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+})
+stopBtn.addEventListener("click", () => {
+    document.removeEventListener("keydown", handleDown);
+    document.removeEventListener("keyup", handleUp);
+    logDiv.textContent = "";
+    stateDiv.textContent = "";
+    stopBtn.disabled = true;
+    startBtn.disabled = false;
+})
+function handleDown(e){
+    logDiv.textContent = `Key ${e.key} is pressed down.`;
+    stateDiv.textContent = "Key is Down";
+}
+function handleUp(e){
+    logDiv.textContent = `Key ${e.key} is pressed up.`;
+    stateDiv.textContent = "Key is Up";
+}
